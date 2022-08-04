3871b1eacecaded6e94ecf6f478e0a094e653352
Olivier Peltre <opeltre@gmail.com>
2022-08-04 12:39:49 +0200

    fix str(Int, Float) removing type annotations

--- fp/instances/num.py
+++ fp/instances/num.py
@@ -3,7 +3,11 @@ from fp.meta import TypeMeta, RingMeta, AlgMeta
 #--- 
 
 class Int(int, metaclass=RingMeta):
-    pass 
+
+    def __str__(self):
+        return super().__repr__()
 
 class Float(float, metaclass=AlgMeta):
-    pass
+    
+    def __str__(self):
+        return super().__repr__()
