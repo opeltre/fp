2a35fdf6edb8b2ac792b28a410599973662b09c0
Olivier Peltre <opeltre@gmail.com>
2024-04-21 09:44:44 +0200

    update examples/foobar

--- examples/foobar.py
+++ examples/foobar.py
@@ -1,20 +1,40 @@
 from fp import *
 from fp.meta import *
 
-import inspect
-
-# List functor 
-x = List(Str)(["abc", "d", "ef"])
+#### List functor ####
+x = List(Str)(["abc", "defgh", "ijkl"])
 y = List(Int)([0, 1, 2])
 
-# Hom bifunctor
+#### Hom bifunctor ####
 foo = Hom(Str, Int)(len)
 bar = Hom(Int, Str)(lambda n: "|" * n)
+
+foo.shows("foo")
+bar.shows("bar")
 
 # composition 
 foobar = foo @ bar
 barfoo = bar @ foo
 
-# Enumerate type class methods
-for k, vk in Hom.methods().items():
-    print(k, ':\t', vk)
+#### Functorial maps ####
+
+x.shows("x")
+List.fmap(bar @ foo)(x).shows("map (bar . foo) x")
+
+### Either type ###
+
+E = Either(Str, Int)
+Ebar = E.fmap(bar)
+Ebar.shows("Either bar")
+
+### Product type
+
+P = Prod(Str, Int)
+p = P(Str("abcd"), Int(8))
+Pf = Prod.fmap(foo, bar)
+Pf.shows("Prod (foo, bar)")
+
+p.shows("x")
+Pf = Prod.fmap(foo, bar).show()
+Pf(p).shows("y")
+
