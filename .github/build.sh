6b4f0c94b89e6e5d9cc53536402e811d31690de2
Olivier Peltre <opeltre@gmail.com>
2024-04-28 17:51:03 +0200

    update build.sh

--- .github/build.sh
+++ .github/build.sh
@@ -21,9 +21,25 @@ function jaxinstall () {
 }
 
 function build() {
+    echo "> Install pip and poetry"
+    echo "..."
+    python -m pip install --upgrade pip
+    python -m pip install pytest poetry poethepoet
+    python -m pip install flake8-future-annotations
+    # exit-zero treats all errors as warnings. The GitHub editor is 127 chars wide
+    flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics
+    echo "..."
+    echo "> Build the fp backage"
     poetry install
-    poetry poe torchinstall
-    poetry poe jaxinstall
+    echo "..."
+    if [[ $1 = *torch* ]]; then
+        echo "> Install torch"
+        poetry poe torchinstall
+        echo "..."
+    fi
+    if [[ $1 = *jax* ]]; then
+        echo "> Install jax"
+        poetry poe jaxinstall
+        echo "..."
+    fi
 }
-
-build $1
