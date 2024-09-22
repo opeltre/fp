30a8a4b31adb51ed09a5c793b54d3861c0eb9a90
Olivier Peltre <opeltre@gmail.com>
2024-09-22 21:35:33 +0200

    test: add recursive State.bind test

--- tests/test_state.py
+++ tests/test_state.py
@@ -4,41 +4,46 @@ from fp import Int, Str
 class TestState: 
 
     St = fp.State(Str)
+    
+    length = St(Int)(lambda s: ("", len(s)))
 
     def test_new(self): 
         StInt = self.St(Int)
         assert isinstance(StInt, fp.Type)
 
-    def test_init(self):
-
-        @self.St(Int)
-        def length(s): 
-            return "", len(s)
-        
-        assert isinstance(length, self.St(Int))
-    
     def test_exec(self): 
-
-        @self.St(Int)
-        def length(s): 
-            return "", len(s)
-
-        assert length.exec("hello world") == ""
+        assert self.length.exec("hello world") == ""
     
     def test_eval(self):
-
-        @self.St(Int)
-        def length(s): 
-            return "", len(s)
-
-        assert length.eval("hello world") == 11
+        assert self.length.eval("hello world") == 11
 
     def test_run_is_call(self): 
+        assert self.length.run("hello") == self.length("hello")
 
-        @self.St(Int)
-        def length(s): 
-            return "", len(s)
+    def test_map(self): 
+        bar = fp.Hom(Int, Str)(lambda n: "|" * n)
+        foobar = self.length.map(bar)
+        assert foobar.eval("hello world") == Str("|||||||||||")
+        assert foobar.exec("hello world") == Str("")
 
-        assert length.run("hello") == length("hello")
+    def test_fmap(self):
+        square = fp.Hom(Int, Int)(lambda n: n**2)
+        length2 = self.St.fmap(square)(self.length)
+        assert length2.eval("hello world") == 121
+    
+    def test_bind(self): 
 
+        @fp.Hom(Int, self.St(Int))
+        def binary(n):
+            q, r = divmod(n, 2) 
+            if r == 1: 
+                digit = self.St(Int)(lambda s: ("|" + s, q))
+            else:
+                digit = self.St(Int)(lambda s: ("." + s, q))
+            # recursive bind
+            return digit.bind(binary) if q > 0 else digit
 
+        binlength = self.length.bind(binary)
+        # 11 = 8 + . + 2 + 1 
+        assert binlength.exec("hello world") == "|.||"
+        
