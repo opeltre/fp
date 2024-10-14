d08816a711862806a8108795f5b614f755d1ad6e
Olivier Peltre <opeltre@gmail.com>
2024-10-14 22:35:50 +0200

    fix: pkg_resources deprecated, use try instead

--- fp/tensors/backend/_pkgs.py
+++ fp/tensors/backend/_pkgs.py
@@ -1,27 +1,21 @@
-import pkg_resources
 import os
 
 import numpy as np
 
-# --- Get available backends ---
-
-pkgs = [p.project_name for p in pkg_resources.working_set]
-
-has_jax = "jax" in pkgs
-has_torch = "torch" in pkgs
-
-del pkgs
-
-# --- Export references ---
-
-if has_jax:
+try:
     import jax
-else:
-    jax = False
 
-if has_torch:
+    has_jax = True
+except ModuleNotFoundError:
+    jax = None
+    has_jax = False
+
+try:
     import torch
-else:
-    torch = False
 
-jnp = has_jax and jax.numpy
+    has_torch = True
+except ModuleNotFoundError:
+    torch = None
+    has_torch = False
+
+jnp = None if jax is None else jax.numpy
