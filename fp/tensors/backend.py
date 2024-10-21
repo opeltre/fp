5008cf999cfceee77602bf7d6eb0f6532c67c7c4
Olivier Peltre <opeltre@gmail.com>
2024-10-21 22:40:35 +0200

    cleanup: single Lift descriptor class

--- fp/tensors/backend.py
+++ fp/tensors/backend.py
@@ -16,15 +16,15 @@ class LiftWrap(Lift):
 
 
 TENSOR_METHODS = [
-    # arithmetic methods
-    LiftWrap("__add__", 2),
-    LiftWrap("__sub__", 2),
-    LiftWrap("__mul__", 2),
-    LiftWrap("__truediv__", 2),
-    LiftWrap("__neg__", 1),
-    # reshapes
-    LiftWrap("flatten", 1),
-    LiftWrap("reshape", lambda T: Hom((T, tuple), T), 0, flip=1),
+    #   # arithmetic methods
+    #   LiftWrap("__add__", 2),
+    #   LiftWrap("__sub__", 2),
+    #   LiftWrap("__mul__", 2),
+    #   LiftWrap("__truediv__", 2),
+    #   LiftWrap("__neg__", 1),
+    #   # reshapes
+    #   LiftWrap("flatten", 1),
+    #   LiftWrap("reshape", lambda T: Hom((T, tuple), T), 0, flip=1),
 ]
 
 # ====== Backend: Wrap an interface ======
@@ -57,13 +57,13 @@ class Backend(Ring, Wrap):
     """
 
     class Object(Wrap.Object):
-        __add__ = LiftWrap("__add__", 2).method()
-        __sub__ = LiftWrap("__sub__", 2).method()
-        __mul__ = LiftWrap("__mul__", 2).method()
-        __neg__ = LiftWrap("__neg__", 1).method()
-        __truediv__ = LiftWrap("__truediv__", 2).method()
-        flatten = LiftWrap("flatten", 1).method()
-        reshape = LiftWrap("reshape", lambda T: Hom((T, tuple), T), 0, flip=1).method()
+        __add__ = LiftWrap(2)
+        __sub__ = LiftWrap(2)
+        __mul__ = LiftWrap(2)
+        __neg__ = LiftWrap(1)
+        __truediv__ = LiftWrap(2)
+        flatten = LiftWrap(1)
+        reshape = LiftWrap(lambda T: Hom((T, tuple), T), lift_args=0, flip=1)
 
     @classmethod
     def new(cls, api: Interface):
