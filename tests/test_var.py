81e4e1ad1ab77a5e2f188d07530d6ccf6d7cf67d
Olivier Peltre <opeltre@gmail.com>
2024-10-24 20:47:59 +0200

    add io.warn

--- tests/test_var.py
+++ tests/test_var.py
@@ -1,5 +1,6 @@
 from fp.meta import Var
-from fp.instances import List, Int
+from fp.cartesian import Hom, Prod
+from fp.instances import List, Int, Str
 
 
 def test_substitute():
@@ -8,6 +9,16 @@ def test_substitute():
 
 def test_substitute_nested():
     assert List("A").substitute({"A": Int}) == List(Int)
+
+
+def test_substitute_hom():
+    assert Hom("A", "A").substitute({"A": Int}) == Hom(Int, Int)
+
+
+def test_substitute_prod():
+    assert Prod(Int, Str, Int) == Prod(Var("A"), Var("B"), Var("A")).substitute(
+        {"A": Int, "B": Str}
+    )
 
 
 def test_match():
